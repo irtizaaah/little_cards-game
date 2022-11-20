fb81c286fbc2326ce8a654d311e8a135abf753b7
irtizaaah <irtizaaah@gmail.com>
2022-11-19 22:40:51 -0800

    font fixes & additional content

--- my-app/src/components/deck/Deck.js
+++ my-app/src/components/deck/Deck.js
@@ -15,18 +15,20 @@ function Deck(props) {
         <div className = "help_button" onClick={handleClickHelp}>
             <h3>{isHelpButtonClicked ? "X" : "?"}</h3>
         </div>
-          <div className={isHelpButtonClicked ? "hide" : "show"} onClick={()=>props.setCurrentDeckIndex(props.index)}>
+        <div className={isHelpButtonClicked ? "hide" : "show"} onClick={()=>props.setCurrentDeckIndex(props.index)}>
           <Link to="/card-page">
             <div className ="image_container">
-                  <img src={require("../../data/" + props.imageURL)} alt="Deck"/>
-              </div>
-              <div className = "text_container">
-                  <h1>{props.name}</h1>
-                  <h2>By {props.author}</h2>
-              </div>
+                <img src={require("../../data/" + props.imageURL)} alt="Deck"/>
+            </div>
+            <div className = "text_container">
+                <h1>{props.name}</h1>
+                <h2>{`${props.author ? "By " + props.author : " "}`}</h2>
+            </div>
           </Link>
-          </div>
-        <div className={isHelpButtonClicked ? "show" : "hide"}>props.summary</div>
+         </div>
+        <div className={isHelpButtonClicked ? "show" : "hide"} onClick={handleClickHelp}>
+          <p className="summary">{props.summary}</p>
+        </div>
     </div>
   );
 }
