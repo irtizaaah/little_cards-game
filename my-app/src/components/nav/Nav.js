b555b15e0005b4a7c4c350c4927e2b8791de388f
irtizaaah <irtizaaah@gmail.com>
2022-11-20 04:21:27 -0800

    Design tweaks

--- my-app/src/components/nav/Nav.js
+++ my-app/src/components/nav/Nav.js
@@ -2,7 +2,6 @@ import './Nav.css';
 import Menu from '../menu/Menu';
 
 import menuIcon from "./menu-icon.png";
-import logo from "./logo.png";
 import {Link} from "react-router-dom";
 import {useState} from "react";
 
@@ -16,9 +15,7 @@ function Nav(props) {
   
   return (
     <div className="Nav">
-        {!props.isDeckPageOpen ? 
-          <img className="menu_img" src={menuIcon} alt="menu icon" onClick={handleClick}/>  : 
-          <img  className="logo_img"src={logo} alt="logo"/>}
+        {!props.isDeckPageOpen ? <img className="menu_img" src={menuIcon} alt="menu icon" onClick={handleClick}/>  : null}
         <Link to="/"><h1>Little Cards</h1></Link>
         {showMenu ? <Menu className="menu" showMenu={showMenu} setShowMenu={setShowMenu} currentDeckIndex={props.currentDeckIndex} /> : null}
     </div>
